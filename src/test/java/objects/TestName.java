81c75f01a4bf78c40f9812436275e83d0ec372e0
HITECH <s12028067@stu.najah.edu>
2023-07-05 02:31:40 +0300

    write TestName class

--- src/test/java/objects/TestName.java
+++ src/test/java/objects/TestName.java
@@ -1,11 +1,31 @@
 package objects;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class TestName {
     private Name name;
     @Before
     public void setup() {
-        name = new Name();
+        name = new Name("Mohammad","Abd-Allateef","Alawneh");
+    }
+    @Test
+    public void testFirstName() {
+        name.setFirstName("Najat");
+        assertEquals("Najat",name.getFirstName());
+    }
+
+    @Test
+    public void testMiddleName() {
+        name.setMiddleName("Sameer");
+        assertEquals("Sameer",name.getMiddleName());
+    }
+
+    @Test
+    public void testLastName() {
+        name.setLastName("Mansour");
+        assertEquals("Mansour",name.getLastName());
     }
 }
